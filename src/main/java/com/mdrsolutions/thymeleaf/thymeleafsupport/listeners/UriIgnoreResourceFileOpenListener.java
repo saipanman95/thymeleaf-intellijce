57db77a104e5d18b1f75e9492eec0d2ea5b204a2
Michael Rodgers <michael.d.rodgers.jr@gmail.com>
2024-09-20 10:59:52 +1000

    added Bus message to IDE user to alert the addition of URI's to ignore file.

--- src/main/java/com/mdrsolutions/thymeleaf/thymeleafsupport/listeners/UriIgnoreResourceFileOpenListener.java
+++ src/main/java/com/mdrsolutions/thymeleaf/thymeleafsupport/listeners/UriIgnoreResourceFileOpenListener.java
@@ -1,6 +1,9 @@
 package com.mdrsolutions.thymeleaf.thymeleafsupport.listeners;
 
 import com.intellij.javaee.ExternalResourceManagerEx;
+import com.intellij.notification.Notification;
+import com.intellij.notification.NotificationType;
+import com.intellij.notification.Notifications;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.fileEditor.FileOpenedSyncListener;
@@ -22,6 +25,7 @@ import static com.intellij.xml.util.HtmlUtil.isHtmlFile;
 public final class UriIgnoreResourceFileOpenListener implements FileOpenedSyncListener {
     private static final Logger logger = Logger.getInstance(UriIgnoreResourceFileOpenListener.class);
     private static final String[] IGNORED_RESOURCES = ThymeleafNamespaceRegistry.getNamespaces().values().toArray( new String[0]);
+    private static final String GROUP_ID = "UriIgnoreResource";
 
     @Override
     public void fileOpenedSync(@NotNull FileEditorManager source, @NotNull VirtualFile file, @NotNull List<FileEditorWithProvider> editorsWithProviders) {
@@ -36,33 +40,32 @@ public final class UriIgnoreResourceFileOpenListener implements FileOpenedSyncLi
 
     public void initializeIgnoredResources() {
         logger.info("UriIgnoreResourceFileOpenListener.initializeIgnoredResources()");
-        ApplicationManager.getApplication().invokeLater(() -> {
-            ApplicationManager.getApplication().runWriteAction(() -> {
-                ExternalResourceManagerEx externalResourceManager = ExternalResourceManagerEx.getInstanceEx();
-                if (externalResourceManager != null) {
+        ApplicationManager.getApplication().invokeLater(() -> ApplicationManager.getApplication().runWriteAction(() -> {
+            ExternalResourceManagerEx externalResourceManager = ExternalResourceManagerEx.getInstanceEx();
+            if (externalResourceManager != null) {
 
-                    List<String> currentIgnoredResources = Arrays.asList(externalResourceManager.getIgnoredResources());
-                    List<String> resourcesToAdd = new ArrayList<>();
+                List<String> currentIgnoredResources = Arrays.asList(externalResourceManager.getIgnoredResources());
+                List<String> resourcesToAdd = new ArrayList<>();
 
-                    // Check if each resource is already ignored
-                    for (String resource : IGNORED_RESOURCES) {
-                        if (!currentIgnoredResources.contains(resource)) {
-                            resourcesToAdd.add(resource);
-                        }
+                // Check if each resource is already ignored
+                for (String resource : IGNORED_RESOURCES) {
+                    if (!currentIgnoredResources.contains(resource)) {
+                        resourcesToAdd.add(resource);
                     }
-
-                    if (!resourcesToAdd.isEmpty()) {
-                        logger.info("Adding ignored resources: " + resourcesToAdd);
-                        CheckedDisposable disposable = Disposer.newCheckedDisposable();
-                        externalResourceManager.addIgnoredResources(resourcesToAdd, disposable);
-                        logger.info("initializeIgnoredResources() - added: " + resourcesToAdd);
-                    } else {
-                        logger.info("All resources are already ignored. No action needed.");
-                    }
-                } else {
-                    logger.info("externalResourceManager is null - operation skipped");
                 }
-            });
-        });
+
+                if (!resourcesToAdd.isEmpty()) {
+                    logger.info("Adding ignored resources: " + resourcesToAdd);
+                    CheckedDisposable disposable = Disposer.newCheckedDisposable();
+                    externalResourceManager.addIgnoredResources(resourcesToAdd, disposable);
+                    logger.info("initializeIgnoredResources() - added: " + resourcesToAdd);
+                    Notifications.Bus.notify(new Notification(GROUP_ID, "DTD and URI Resources", "Related Thymeleaf Layout Namespaces have been safely ignored.", NotificationType.INFORMATION));
+                } else {
+                    logger.info("All resources are already ignored. No action needed.");
+                }
+            } else {
+                logger.info("externalResourceManager is null - operation skipped");
+            }
+        }));
     }
 }
