89df12293a93df2f2a3385fc968efb2e8cb181d6
Maksymilian <99613189+maksik997@users.noreply.github.com>
2025-06-02 12:53:46 +0200

    fix: Update 'untilBuild' property in build.gradle.kts.

--- build.gradle.kts
+++ build.gradle.kts
@@ -42,7 +42,7 @@ tasks {
 
     patchPluginXml {
         sinceBuild.set("231.8109.175")  // Match this to the actual IntelliJ version
-        untilBuild.set("243.*")
+        untilBuild.set("999.*")
     }
 
     signPlugin {
